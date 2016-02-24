c293e3ee8ffc50c8bfe3d28a594d1ffef29010d8
detrevni <noommaerd@gmail.com>
2016-02-24 19:49:49 +0300

    Create 1296.cpp

---
+++ 1296.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+#include <cstdio>
+#include <climits>
+#include <iomanip>
+#include <cmath>
+#include <ctime>
+#include <map>
+#include <set>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <cstring>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <numeric>
+#include <functional>
+#include <bitset>
+#include <deque>
+
+using namespace std;
+
+#if __cplusplus >= 201103
+    #include <unordered_map>
+    #include <future>
+    #include <atomic>
+    #include <chrono>
+    #include <unordered_set>
+    #include <array>
+    #include <random>
+    #include <mutex>
+#endif
+
+#define pii pair<int, int>
+
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+
+#define f first
+#define s second
+
+typedef long long ll;
+typedef long double ld;
+
+const int inf = (int)1e9 + 7;
+const int linf = (int)1e16 + 7;
+const int maxn = (int)4e5 + 7;
+
+const ld eps = ld(1e-11);
+
+int n, mn, s, ans;
+int a[maxn];
+
+main(){
+    scanf("%d", &n);
+    for(int i = 1; i <= n; ++i) scanf("%d", &a[i]);
+    for(int i = 1; i <= n; ++i){
+        s += a[i];
+        ans = max(ans, s - mn);
+        mn = min(mn, s);
+    }
+    printf("%d", ans);
+    return 0;
+}
