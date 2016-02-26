5f635548ea4e948a5f6ffd19036769c189e79d79
detrevni <noommaerd@gmail.com>
2016-02-26 06:55:45 +0300

    Create 1586.cpp

---
+++ 1586.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+#include <cstdio>
+#include <climits>
+#include <iomanip>
+#include <cmath>
+#include <ctime>
+#include <map>
+#include <set>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <cstring>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <numeric>
+#include <functional>
+#include <bitset>
+#include <deque>
+
+using namespace std;
+
+#if __cplusplus >= 201103
+    #include <unordered_map>
+    #include <future>
+    #include <atomic>
+    #include <chrono>
+    #include <unordered_set>
+    #include <array>
+    #include <random>
+    #include <mutex>
+#endif
+
+#define pii pair<int, int>
+
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+
+#define f first
+#define s second
+
+typedef long long ll;
+typedef long double ld;
+
+const int inf = (int)1e9 + 9;
+const int linf = (int)1e16 + 7;
+const int maxn = (int)2e5 + 7;
+const int lmaxn = (int)1e6 + 7;
+
+const ld eps = ld(1e-11);
+
+inline bool Prime(int x){
+    if(x < 2) return 0;
+    for(int i = 2; i * i <= x; ++i)
+        if(x % i == 0)
+            return 0;
+    return 1;
+}
+
+int n, ans;
+int d[10001][101];
+
+main(){
+    scanf("%d", &n);
+    if(n == 3){
+        printf("143");
+        return 0;
+    }
+    for(int i = 101; i <= 999; ++i){
+        if(Prime(i))
+            d[3][i % 100]++;
+    }
+    for(int i = 4; i <= n; ++i){
+        for(int l = 10; l <= 99; ++l){
+            for(int j = 1; j <= 9; ++j){
+                if(Prime(j * 100 + l))
+                    d[i][l] += d[i - 1][(j * 10 + l / 10)], d[i][l] %= inf;
+            }
+        }
+    }
+    for(int i = 10; i <= 99; ++i)
+        ans += d[n][i], ans %= inf;
+    printf("%d", ans);
+    return 0;
+}
